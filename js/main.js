21f4e6bac4017c0a030d57c24248c9e4833c135e
Gulchachak <ilkaeva85@rambler.ru>
2020-08-20 23:00:11 -0500

    third commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 new WOW().init();
+var mySwiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".projects-pagination",
+    bulletClass: "projects-bullets",
+    bulletActiveClass: "projects-bullets-active",
+    clickable: true,
+  },
+});
